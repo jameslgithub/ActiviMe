e3c89704c187cf8a77f8e8a141c2795cff478857
Vlad Tepes <abaraian0@gmail.com>
2024-03-11 20:02:04 -0400

    fixed json errors with nonexisting properties

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.post("/search",(req,res)=>{ //user searches for location and activity
    let top5DestinationInfo=[] //response from text search
    console.log("recieved on search enter "); //show that server got client
    //still need to add conditionals
-
+    
    //now send to google text search new on google places api
    fetch("https://places.googleapis.com/v1/places:searchText",{
     method:"POST",
@@ -31,27 +31,32 @@ app.post("/search",(req,res)=>{ //user searches for location and activity
         'X-Goog-FieldMask': 'places.id,places.name,places.displayName,places.formattedAddress,places.priceLevel,places.location,places.editorialSummary,places.photos',
     },
     body: JSON.stringify({
-        "textQuery": req.body["search"]+req.body["location"],
+        "textQuery": req.body["search"]+" in "+req.body["location"], //in forces a list of all activities may have to parse out user input to prevent further errors
         "languageCode":"en"
     })
    }).then((response)=>response.json())
    .then((data)=>{
+    console.log(data)
     for(let i =0; i<5;i++){
-        top5DestinationInfo.push({
-            "name": data["places"][i]["displayName"]["text"],
-        //     "priceLevel":data["places"][i]["priceLevel"],
-            "address": data["places"][i]["formattedAddress"],
-        //     "latitidue":data["places"][i]["location"]["latitude"],
-        //     "longitude":data["places"][i]["location"]["longitude"],
-        //     "summary":data["places"][i]["editorialSummary"]["text"],
-            // "photoUri":data["places"][i]["photos"][0]["authorAttributions"][0]["photoUri"],
-        })
-        top5dests.push(data["places"][i]["displayName"]["text"])
+        if (data["places"][i]){
+        top5DestinationInfo.push({  //using optional chaining to error check for properties that may or may not exist
+            "name": data.places[i]?.displayName?.text,
+            "priceLevel":data.places[i]?.priceLevel,
+            "address": data.places[i]?.formattedAddress,
+             "latitidue":data.places[i]?.location?.latitude,
+            "longitude":data.places[i]?.location?.longitude,
+             "summary":data.places[i]?.editorialSummary?.text,
+             "photoUri":data.places[i]?.photos?.[0]?.authorAttributions?.[0]?.photoUri,
+        }
+        )
     }
-    // console.log(top5dests)
+        
+       
+    }
+     //console.log(top5DestinationInfo)
 
     res.json({ "places":top5DestinationInfo})
-    // console.log(top5DestinationInfo)
+   
    })
 })
 
